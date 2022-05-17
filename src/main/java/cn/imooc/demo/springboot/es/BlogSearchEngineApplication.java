aa6b46f69376a4a7db904e8515572f4b81511d82
z1514 <2498973668@qq.com>
2022-05-16 18:31:48 -0800

    init

---
+++ src/main/java/cn/imooc/demo/springboot/es/BlogSearchEngineApplication.java
@@ -0,0 +1,15 @@
+package cn.imooc.demo.springboot.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchEngineApplication {
+
+    //最全笔记：https://www.yuque.com/gaohanghang/vx5cb2/aa576g
+    //代码地址：https://github.com/gaohanghang/springboot-blog-es
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/imooc/demo/springboot/es/BlogSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.imooc.demo.springboot.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
